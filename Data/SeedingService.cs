381f7385019266995f5b551a97475b05b2501e4c
Franz Fialho <franzfialho@gmail.com>
2021-02-12 22:12:11 -0300

    SellerController

--- Data/SeedingService.cs
+++ Data/SeedingService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using SalesWebMVC.Models;
 using SalesWebMVC.Models.Enums;
+using System.Globalization;
 
 namespace SalesWebMVC.Data
 {
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC
 {
@@ -41,7 +42,11 @@ namespace SalesWebMVC
                      builder.MigrationsAssembly("SalesWebMVC")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
