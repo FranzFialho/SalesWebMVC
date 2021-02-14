c36f501a48e8802e8530d85913fa718e6df9f91b
Franz Fialho <franzfialho@gmail.com>
2021-02-14 03:18:58 -0300

    Delete Seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -43,5 +43,29 @@ namespace SalesWebMVC.Controllers
             _sellersService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellersService.FindById(id.Value);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellersService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
